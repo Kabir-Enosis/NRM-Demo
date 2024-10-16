22db76052a7eb701bc9a820f9cd6430c74ff070e
kabir-Enosis <kabir.ahmed@enosisbd.com>
2024-10-16 17:24:11 +0600

    NgRx implemented

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DrawerModule } from '@progress/kendo-angular-layout';
+import { AppBarModule } from '@progress/kendo-angular-navigation';
+import { ButtonModule } from '@progress/kendo-angular-buttons';
+import { InputsModule, TextBoxModule } from '@progress/kendo-angular-inputs';
+import { CommonModules } from '../commonModule';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { selectTab } from '../store/tab.action';
+import { getSelectedTab } from '../store/tab.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [DrawerModule, AppBarModule, ButtonModule, InputsModule, CommonModules, RouterOutlet, TextBoxModule, CommonModule ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+  tabs = ['Dashboard', 'Profile', 'Settings'];
+  selectedTab$!: Observable<string> 
+
+  constructor(private store: Store) {
+    this.selectedTab$ = this.store.select(getSelectedTab);
+    this.store.dispatch(selectTab({tab: 'Dashboard'}))
+
+  }
+
+  ngOnInit() {
+    this.selectedTab$.subscribe((tab1) => {
+      console.log('Current selected tab:', tab1);
+    });
+    
+  }
+
+  tabChanged(tab: string) {
+    this.store.dispatch(selectTab({ tab }));
+  }
 
 }
