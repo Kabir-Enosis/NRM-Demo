5419d0b4817787335a10c21d1f17092efa3729e6
kabir-Enosis <kabir.ahmed@enosisbd.com>
2024-10-18 12:29:26 +0600

    added tab icons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModules } from '../commonModule';
+import { LayoutModule } from "@progress/kendo-angular-layout";
+import { IndicatorsModule } from "@progress/kendo-angular-indicators";
+import { IconsModule } from "@progress/kendo-angular-icons";
+import { NavigationModule } from "@progress/kendo-angular-navigation";
+import {
+  bellIcon,
+  menuIcon,
+  searchIcon,
+  SVGIcon,
+} from "@progress/kendo-svg-icons";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModules],
+  imports: [CommonModules, IndicatorsModule, IconsModule, NavigationModule, LayoutModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  public menuIcon: SVGIcon = menuIcon;
+  public searchIcon: SVGIcon = searchIcon;
+  public bellIcon: SVGIcon = bellIcon;
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,17 +10,32 @@ import { Store } from '@ngrx/store';
 import { selectTab } from '../store/tab.action';
 import { getSelectedTab } from '../store/tab.selector';
 import { Observable } from 'rxjs';
+import { IconsModule } from "@progress/kendo-angular-icons";
+import {
+  homeIcon,
+  graphIcon,
+  chartColumnStackedIcon,
+  tableCellPropertiesIcon,
+  userIcon,
+  SVGIcon,
+} from "@progress/kendo-svg-icons";
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [DrawerModule, AppBarModule, ButtonModule, InputsModule, CommonModules, RouterOutlet, TextBoxModule, CommonModule ],
+  imports: [DrawerModule, AppBarModule, ButtonModule, InputsModule, CommonModules, RouterOutlet, TextBoxModule, CommonModule, IconsModule ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent implements OnInit{
   tabs = ['Home', 'Analytics', 'Charts', 'Graphs'];
   selectedTab$!: Observable<string> 
+  public homeIcon: SVGIcon = homeIcon;
+  public analyticsIcon: SVGIcon = graphIcon;
+  public graphsIcon: SVGIcon = chartColumnStackedIcon;
+  public chartsIcon: SVGIcon = tableCellPropertiesIcon;
+  public userIcon: SVGIcon = userIcon;
 
   constructor(private store: Store, private router: Router) {
     this.selectedTab$ = this.store.select(getSelectedTab);
@@ -38,6 +53,21 @@ export class SidebarComponent implements OnInit{
   tabChanged(tab: string) {
     this.store.dispatch(selectTab({ tab }));
     this.router.navigate([`/dashboard/${tab.toLowerCase()}`]);
+  }
+
+  getIcon(tab: string): SVGIcon {
+    switch (tab) {
+      case 'Home':
+        return this.homeIcon;
+      case 'Analytics':
+        return this.analyticsIcon;
+      case 'Charts':
+        return this.chartsIcon;
+      case 'Graphs':
+        return this.graphsIcon;
+      default:
+        return this.homeIcon; 
+    }
   }
 
 }
