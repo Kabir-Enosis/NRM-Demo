4ea4e2facff266cd73844cb128ddeeaf04a67de1
kabir-Enosis <kabir.ahmed@enosisbd.com>
2024-10-18 15:46:30 +0600

    if else if block

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -56,19 +56,16 @@ export class SidebarComponent implements OnInit{
   }
 
   getIcon(tab: string): SVGIcon {
-    switch (tab) {
-      case 'Home':
-        return this.homeIcon;
-      case 'Analytics':
+    if(tab === 'Home')
+      return this.homeIcon;
+    else if(tab === 'Analytics') 
         return this.analyticsIcon;
-      case 'Charts':
+    else if(tab ===  'Charts')
         return this.chartsIcon;
-      case 'Graphs':
+    else if(tab ===  'Graphs')
         return this.barGraphsIcon;
-      default:
-        return this.homeIcon; 
-        
-    }
+      
+    return this.homeIcon; 
   }
 
 }
